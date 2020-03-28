eff51175e23e31d5c7457377cb55cb5c7a2e4eed
Prabath Perera <dryize@gmail.com>
2020-03-28 19:56:57 +0530

    demo project updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngx-progressbar';
+  value = 0;
+  interval;
+
+  ngOnInit(){
+    this.interval = setInterval(() => {
+      this.value += 2;
+      if(this.value==100)clearInterval(this.interval);
+    },20)
+  }
 }
